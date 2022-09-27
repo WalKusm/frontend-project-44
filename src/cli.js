d85ad289f02f9ef8c623ba3aa08270f1a88ca7c0
Валерия Кузьмина <cle.ra.2012@mail.ru>
2022-09-27 11:39:05 +0300

    step 3

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return(`Hello, ${userName}!`);
-}
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return (`Hello, ${userName}!`);
+};
+export default greeting;
