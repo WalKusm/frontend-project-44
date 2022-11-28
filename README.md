1606838e4799afbe677e74971d94333d694959cb
Валерия Кузьмина <cle.ra.2012@mail.ru>
2022-11-28 16:02:23 +0300

    step 9

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 https://asciinema.org/a/67sBca3qU4bG1pu0sq6oZ2gmF (asciinema brain-cgd)
 https://asciinema.org/a/Ugbx6jljwyv6D2LWxRy0BaiBI (asciinema brain-progression)
+ https://asciinema.org/a/QQsbi9dHpUDH1B9AbHrlfk96J (asciinema brain-prime)
---
+++ src/prime.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+
+const game5 = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
+  let i = 0;
+  while (i < 3) {
+    const count1 = Math.abs((Math.random() * 100).toFixed(0));
+    let result = 0;
+
+    let j = 0;
+    let n = 0;
+    while (j < count1) {
+      if (count1 % (count1 - j) === 0) {
+        n += 1;
+      }
+      j += 1;
+    }
+
+    if (n === 2) {
+      result = 'yes';
+    } else {
+      result = 'no';
+    }
+
+    console.log(`Question: ${count1}`);
+    const answ = readlineSync.question('Your answer: ');
+
+    if (answ === result && i === 2) {
+      console.log(`Correct!\nCongratulations, ${userName}!`);
+      i += 1;
+    }
+    if (answ === result && i < 2) {
+      console.log('Correct!');
+      i += 1;
+    }
+    if (answ !== result) {
+      i = 3;
+      console.log(`'${answ}' is wrong answer ;(. Correct answer was '${result}'.\n Let's try again, ${userName}!`);
+    }
+  }
+};
+export default game5();
