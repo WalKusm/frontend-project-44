5363cc5a4f4c7b1b73da67b9a5e3662b285333a6
Валерия Кузьмина <cle.ra.2012@mail.ru>
2023-02-03 20:57:15 +0300

    change if-else-construction to switch

--- src/games/calc.js
+++ src/games/calc.js
@@ -4,17 +4,16 @@ import getRandomNumber from '../random-number.js';
 const operatorArray = ['+', '-', '*'];
 
 const getCalculation = (count1, count2, operator) => {
-  let answ = 0;
-  if (operator === '+') {
-    answ = Number(count1) + Number(count2);
+  switch (operator) {
+    case '+':
+      return Number(count1) + Number(count2);
+    case '-':
+      return Number(count1) - Number(count2);
+    case '*':
+      return Number(count1) * Number(count2);
+    default:
+      return false;
   }
-  if (operator === '-') {
-    answ = Number(count1) - Number(count2);
-  }
-  if (operator === '*') {
-    answ = Number(count1) * Number(count2);
-  }
-  return (String(answ));
 };
 
 const getQuestionAndAnswer = () => {
@@ -22,7 +21,7 @@ const getQuestionAndAnswer = () => {
   const count2 = getRandomNumber(100);
   const operator = operatorArray[Math.floor(Math.random() * 3)];
   const question = `${count1} ${operator} ${count2}`;
-  const answer = getCalculation(count1, count2, operator);
+  const answer = String(getCalculation(count1, count2, operator));
   return [question, answer];
 };
 
